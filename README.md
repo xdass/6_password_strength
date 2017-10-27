9c7b1ea0c010c732178a5c19090d30d709655102
xdass <420600@gmail.com>
2017-10-27 20:04:53 +0500

    Init commit

--- README.md
+++ README.md
@@ -2,6 +2,41 @@
 
 [TODO. There will be project description]
 
+# How to start program
+To start the script you need a blacklist.txt file, you'll find it on github <br>
+
+**On linux:**
+```bash
+$ python password_strength.py
+```
+**On windows:**<br>
+The same as linux
+
+# Examples of the program
+```
+$ python password_strength.py
+Введите пароль: qwerty
+Очень слабый пароль
+```
+
+```
+$ python password_strength.py
+Введите пароль: Dmitry
+Слабый пароль
+```
+
+```
+$ python password_strength.py
+Введите пароль: Dmitry123
+Хороший пароль
+```
+
+```
+$ python password_strength.py
+Введите пароль: Dmitry123@
+Очень крутой пароль
+```
+
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,43 @@
+import re
+from string import punctuation
 
 
-def get_password_strength(password):
-    pass
+def load_passwords_blacklist(filename):
+    try:
+        with open(filename) as file:
+            bad_passwords = file.read().split('\n')
+            return bad_passwords
+    except FileNotFoundError:
+        return None
 
+
+def get_password_strength(password, bad_passwords):
+    password_strength_points = 1
+    if password in bad_passwords:
+        return password_strength_points
+    if re.search(r'[A-Z]+', password) and re.search(r'[a-z]+', password):
+        password_strength_points += 3
+    else:
+        password_strength_points += 2
+    if re.search(r'\d+', password):
+        password_strength_points += 3
+    if re.search(r'[{}]+'.format(punctuation), password):
+        password_strength_points += 3
+    return password_strength_points
+
+
+def print_password_strength(password_strength):
+    if password_strength == 1:
+        print('Очень слабый пароль')
+    if (password_strength > 1) and (password_strength < 6):
+        print('Слабый пароль')
+    if (password_strength > 6) and (password_strength < 10):
+        print('Хороший пароль')
+    if password_strength == 10:
+        print('Очень крутой пароль')
 
 if __name__ == '__main__':
-    pass
+    user_password = input('Введите пароль: ')
+    blacklist = load_passwords_blacklist('blacklist.txt')
+    password_points = get_password_strength(user_password, blacklist)
+    print_password_strength(password_points)
